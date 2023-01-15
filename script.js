d18d50e3753025b13f190c131f732418d242e9e3
Nitish <66010219+Nitishz@users.noreply.github.com>
2023-01-15 09:09:58 +0530

    Added One Brick

---
+++ script.js
@@ -0,0 +1,70 @@
+let paddle_x, paddle_y, paddle_width, paddle_height, paddle_dx;
+let brick_x, brick_y, brick_width, brick_height;
+let ball_x, ball_y, ball_diameter, ball_dx, ball_dy;
+
+function setup() {
+  createCanvas(400, 400);
+  background("black");
+  paddle_width = 100;
+  brick_width = 130;
+  paddle_x = (width / 2) - (paddle_width / 2);
+  paddle_y = height - 25;
+  brick_x = (width / 2) - (brick_width / 2);
+  brick_y = 100;
+  paddle_height = 15;
+  brick_height = 15;
+  
+  ball_diameter = 20;
+  ball_dx = 1;
+  ball_dy = 2;
+  paddle_dx = 3;
+  ball_x = (width / 2) - (ball_diameter / 2);
+  ball_y = (height / 2) - (ball_diameter / 2);
+}
+
+function draw() {
+  background("black");
+  if (ball_x + (ball_diameter / 2) > width) {
+    ball_dx = -ball_dx;
+  }
+  
+  if (ball_x - (ball_diameter / 2) < 0) {
+    ball_dx = -ball_dx;
+  }
+  
+  if (ball_y + (ball_diameter / 2) > height) {
+    ball_dx = 0;
+    ball_dy = 0;
+  }
+  
+  if (ball_y - (ball_diameter / 2) < 0) {
+    ball_dy = -ball_dy;
+  }
+
+  ball_x = ball_x + ball_dx;
+  ball_y = ball_y + ball_dy;
+  
+  if (keyIsDown(LEFT_ARROW)) {
+    paddle_x = paddle_x - paddle_dx;
+  }
+  
+  if (keyIsDown(RIGHT_ARROW)) {
+    paddle_x = paddle_x + paddle_dx;
+  }
+  
+  if ((ball_x < paddle_x + paddle_width) && 
+     (ball_x > paddle_x) && (ball_y < paddle_y + (paddle_height / 2) + ball_diameter + 30) && (ball_y > paddle_y)) {
+    ball_dy = -ball_dy;
+   }
+  
+  if ((ball_x < brick_x + brick_width) && 
+     (ball_x > brick_x) && (ball_y < brick_y + (brick_height / 2) + ball_diameter + 10) && (ball_y > brick_y)) {
+    brick_x = 1000;
+    brick_y = 1000;
+    ball_dy = -ball_dy;
+   }
+  
+  circle(ball_x, ball_y, ball_diameter);
+  rect(paddle_x, paddle_y, paddle_width, paddle_height);
+  rect(brick_x, brick_y, brick_width, brick_height);
+}
